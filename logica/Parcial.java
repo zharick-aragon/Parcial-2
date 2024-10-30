8a23d00bf01af540f709ac849a6039ee758a7ded
zharick-aragon <z.aragon@uniandes.edu.co>
2024-10-30 09:20:13 -0500

    Pruebas

--- logica/Parcial.java
+++ logica/Parcial.java
@@ -3,7 +3,7 @@ package logica;
 //* Reglas matematicas no se pude dividir por cero */
 //(-b±√(b²-4ac))/(2a)
 public class Parcial {
-    public double cuadratica(int a, int b , int c) throws Exception {
+    public double cuadratica1(int a, int b , int c) throws Exception {
         if (a == 0){ throw new Exception("Si el coeficiente a es 0, automaticamente deja de ser una cuadratica"); }
         if (c== 0){ throw new Exception("Utilizando la ecuacion de la cuadratica no se puede dividir por cero"); }
         else {
@@ -13,12 +13,29 @@ public class Parcial {
             if (m>n){ throw new Exception("Utilizando la formula de la cuadratica, la raiz no puede ser negativa"); }
             else{
                 double raiz = Math.pow((n-m), (1/2)); 
-            return (-b +(raiz))/divisor;
+            return Math.round(((-b +(raiz))/divisor));
             //return (-b -(raiz))/divisor;
             }
         }
     }
-}
+
+    public double cuadratica2(int a, int b , int c) throws Exception {
+        if (a == 0){ throw new Exception("Si el coeficiente a es 0, automaticamente deja de ser una cuadratica"); }
+        if (c== 0){ throw new Exception("Utilizando la ecuacion de la cuadratica no se puede dividir por cero"); }
+        else {
+            double m = 4*a*c; 
+            double n = Math.pow(b, 2); 
+            double divisor = 2*a; 
+            if (m>n){ throw new Exception("Utilizando la formula de la cuadratica, la raiz no puede ser negativa"); }
+            else{
+                double raiz = Math.pow((n-m), (1/2)); 
+            return Math.round(-b -(raiz))/divisor;
+            //return (-b -(raiz))/divisor;
+            }
+        }
+    }
+        
+    }
 
 
 
--- logica/Pruebas.java
+++ logica/Pruebas.java
@@ -1,9 +1,23 @@
 package logica;
 import logica.Parcial;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
 
 public class Pruebas {
-
-
+    public Parcial parcial = new Parcial(); 
+    @Test
+    void cuadratica1() throws Exception{
+        assertEquals(1, parcial.cuadratica1(3,-6,3));
+        assertEquals(-1, parcial.cuadratica1(1,3,2));
+        assertEquals("Si el coeficiente a es 0, automaticamente deja de ser una cuadratica", parcial.cuadratica1(0,2,3));
+    }
+    @Test
+    void cuadratica2()throws Exception{
+        assertEquals(-2, parcial.cuadratica2(1,3,2));
+    }
 }
 
+/*Desarrolle una suite de pruebas para el ejercicio del punto 1 que asegure 100% de cobertura de la clase Parcial
+
+Incluya la suite en el mismo proyecto en repositorio GIT del punto 1 */
